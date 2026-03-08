c43e7ef3f5d1fa49218c7fe9245beee48dffeb60
Frank Aka <157696799+wxvz@users.noreply.github.com>
2026-03-08 16:23:07 +0000

    Added sign up logic and will later add hash encryption

--- ServiceBookingApp/CustomerSignup.xaml.cs
+++ ServiceBookingApp/CustomerSignup.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,7 +33,32 @@ namespace ServiceBookingApp
 
         private void SignUpBtn_Click(object sender, RoutedEventArgs e)
         {
-            // Will implement the logic to sign up the customer here later
+            if (passwordPBX.Password != confirmPasswordPBX.Password)
+            {
+                MessageBox.Show("Passwords do not match!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            using (ServiceBookingContext db = new ServiceBookingContext())
+            {
+                var newCustomer = new Customer
+                {
+                    Name = firstNameTBX.Text + " " + lastNameTBX.Text,
+                    Email = emailTBX.Text,
+                    Address = string.Empty,
+                    PhoneNumber = phoneTBX.Text,
+                    Password = passwordPBX.Password // TODO: Hash with BCrypt
+                };
+
+                db.Customers.Add(newCustomer);
+                db.SaveChanges();
+
+                MessageBox.Show("Account created successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                CustomerLogin customerLogin = new CustomerLogin();
+                customerLogin.Show();
+                this.Close();
+            }
         }
     }
 }
