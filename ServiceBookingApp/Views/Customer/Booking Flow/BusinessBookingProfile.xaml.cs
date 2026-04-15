90efd13dc2b4dc4912c15e2bf6975c26f56ec014
Frank Aka <fakaman47@gmail.com>
2026-04-15 21:33:17 +0100

    Namespace update

--- ServiceBookingApp/Views/Customer/Booking Flow/BusinessBookingProfile.xaml.cs
+++ ServiceBookingApp/Views/Customer/Booking Flow/BusinessBookingProfile.xaml.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using ServiceBookingApp.Data;
+
 
 namespace ServiceBookingApp.Views.Customer.Booking_Flow
 {
