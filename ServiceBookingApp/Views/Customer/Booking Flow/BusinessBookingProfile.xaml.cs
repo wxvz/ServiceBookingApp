15b5ae185a15bcbb62728776555fecab9abceef3
Frank Aka <fakaman47@gmail.com>
2026-04-15 20:35:19 +0100

    Business Booking Profile

--- ServiceBookingApp/Views/Customer/Booking Flow/BusinessBookingProfile.xaml.cs
+++ ServiceBookingApp/Views/Customer/Booking Flow/BusinessBookingProfile.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ServiceBookingApp.Models;
 
 namespace ServiceBookingApp.Views.Customer.Booking_Flow
 {
@@ -21,9 +11,63 @@ namespace ServiceBookingApp.Views.Customer.Booking_Flow
     /// </summary>
     public partial class BusinessBookingProfile : Page
     {
-        public BusinessBookingProfile()
+        private readonly int _businessId;
+        private ServiceBookingContext db = new ServiceBookingContext();
+
+        public BusinessBookingProfile(int businessId)
         {
             InitializeComponent();
+            _businessId = businessId;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadBusinessProfile();
+        }
+
+        private void LoadBusinessProfile()
+        {
+            try
+            {
+                var business = db.Businesses.FirstOrDefault(b => b.BusinessId == _businessId);
+                var services = db.Services.Where(s => s.BusinessId == _businessId && s.IsActive).ToList();
+
+                if (business != null)
+                {
+                    BusinessNameText.Text = business.Name;
+                    BusinessDescriptionText.Text = business.Description;
+                    BusinessContactText.Text = $"{business.Address} | {business.PhoneNumber} | {business.Email}";
+                }
+
+                if (services.Count > 0)
+                {
+                    ServicesItemsControl.ItemsSource = services;
+                    ServicesItemsControl.Visibility = Visibility.Visible;
+                    NoServicesText.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    ServicesItemsControl.Visibility = Visibility.Collapsed;
+                    NoServicesText.Visibility = Visibility.Visible;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading business profile: {ex.Message}");
+            }
+        }
+
+        private void BookService_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is int serviceId)
+            {
+                NavigationService.Navigate(new ConfirmBookingPage(serviceId));
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
         }
     }
 }
