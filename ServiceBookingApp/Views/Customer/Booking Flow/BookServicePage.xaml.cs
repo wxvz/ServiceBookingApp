bc681c2ec0f3784cec3b9761535aace7b02edbb0
Frank Aka <fakaman47@gmail.com>
2026-04-15 20:33:49 +0100

    Book Business Service Page

--- ServiceBookingApp/Views/Customer/Booking Flow/BookServicePage.xaml.cs
+++ ServiceBookingApp/Views/Customer/Booking Flow/BookServicePage.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ServiceBookingApp.Models;
 
 namespace ServiceBookingApp.Views.Customer.Booking_Flow
 {
@@ -21,9 +10,47 @@ namespace ServiceBookingApp.Views.Customer.Booking_Flow
     /// </summary>
     public partial class BookServicePage : Page
     {
+        private ServiceBookingContext db = new ServiceBookingContext();
+
         public BookServicePage()
         {
             InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadBusinesses();
+        }
+
+        private void LoadBusinesses()
+        {
+            try
+            {
+                var businesses = db.Businesses.ToList();
+                if (businesses.Count > 0)
+                {
+                    BusinessesItemsControl.ItemsSource = businesses;
+                    NoBusinessesText.Visibility = Visibility.Collapsed;
+                    BusinessesItemsControl.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    NoBusinessesText.Visibility = Visibility.Visible;
+                    BusinessesItemsControl.Visibility = Visibility.Collapsed;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading businesses: {ex.Message}");
+            }
+        }
+
+        private void ViewServices_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is int businessId)
+            {
+                this.NavigationService.Navigate(new BusinessBookingProfile(businessId));
+            }
         }
     }
 }
